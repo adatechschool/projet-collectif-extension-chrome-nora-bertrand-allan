94ef16519f5217b349c41eab9698454e51f29d11
Bertrand MARIE <photomb09@gmail.com>
2024-04-30 15:56:07 +0200

    recup data APIs

--- js/script.js
+++ js/script.js
@@ -1,34 +1,37 @@
 const toDay = Math.round(Date.now()/1000);
-let lat = ""
-let long = ""
 
+//SUN API
 
-async function getSun(){
-    while (lat === null && long === null) { 
-    const reponse =  await fetch("http://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today");
-    const resultSun = await reponse.json();
+setTimeout(() => {
+    async function getSun(){
+        const reponse =  await fetch("https://api.sunrise-sunset.org/json?lat="+coordArr[0]+"&lng="+coordArr[1]+"&date="+toDay);
+        const resultSun = await reponse.json();
+        return resultSun
     }
-    return resultSun
-} 
+
+    getSun().then(
+        resultSun =>{
+            console.log(resultSun)
+        }
+    )}, "5000");
 
 
+//Prommesse en .then en cascade pour attente de la recupération des coordonnées GPS pour introduction dans l'API Sun.
+
+
+//MOON API
 async function getMoonPhase(){
     const reponse =  await fetch("http://api.farmsense.net/v1/moonphases/?d="+toDay);
     const resultMoon = await reponse.json();
     return resultMoon
 }
 
-getSun().then(
-    resultSun =>{
-        console.log(resultSun)
-    }
-) 
-
 getMoonPhase().then(result => {
     console.log(result)
 })
 
 
+//COORDONNEES NAVIGATEUR
 function gps(){
     if (navigator.geolocation){
         navigator.geolocation.getCurrentPosition(stockPosition)
@@ -36,13 +39,11 @@ function gps(){
         console.log("error")
     }
 }
-
+let coordArr = [];
 function stockPosition(position){
-     lat = position.coords.latitude;
-     long = position.coords.longitude;
-     console.log(lat)
-     console.log(long)
-    
-     
-}
-gps()
+     const lat = position.coords.latitude;
+     const long = position.coords.longitude;
+     coordArr.push(lat, long);
+     console.log(coordArr);
+};
+gps();
