7a3a1eb12d5b6f1846a4db1cf954937ab574143f
Allan <allan.delatuillerie@gmail.com>
2024-05-02 12:41:21 +0200

    v0.00032

--- js/script.js
+++ js/script.js
@@ -1,33 +1,47 @@
-const toDay = Math.round(Date.now()/1000);
+const toDayMoon = Math.round(Date.now()/1000);
+const toDaySun = new Date;
+const year = toDaySun.getUTCFullYear();
+const month = toDaySun.getUTCMonth()+1;
+const day = toDaySun.getUTCDate();
+const fullDateForSun = year + "-" + month + "-" + day;
+const balisePhase = document.getElementById("phase")
+const baliseSunset = document.getElementById("sunset")
+const baliseSunrise = document.getElementById("sunrise")
 
 //SUN API
 
 setTimeout(() => {
     async function getSun(){
-        const reponse =  await fetch("https://api.sunrise-sunset.org/json?lat="+coordArr[0]+"&lng="+coordArr[1]+"&date="+toDay+"&tzid=Europe/Paris");
+        const reponse =  await fetch("https://api.sunrise-sunset.org/json?lat="+coordArr[0]+"&lng="+coordArr[1]+"&tzid=Europe/Paris&formatted=0");
         const resultSun = await reponse.json();
         return resultSun
     }
 
     getSun().then(
         resultSun =>{
-            console.log(resultSun)
+            console.log(resultSun);
+            const sunset = resultSun.results.sunset.slice(11,19)
+            const sunrise = resultSun.results.sunrise.slice(11,19)
+            baliseSunrise.innerHTML = sunrise
+            baliseSunset.innerHTML = sunset
+
         }
-    )}, "5000");
-
-
-//Prommesse en .then en cascade pour attente de la recupération des coordonnées GPS pour introduction dans l'API Sun.
-
-
-//MOON API
-async function getMoonPhase(){
-    const reponse =  await fetch("http://api.farmsense.net/v1/moonphases/?d="+toDay);
-    const resultMoon = await reponse.json();
-    return resultMoon
-}
-
-getMoonPhase().then(result => {
-    console.log(result)
+    )});
+    
+    
+    //Prommesse en .then en cascade pour attente de la recupération des coordonnées GPS pour introduction dans l'API Sun.
+    
+    
+    //MOON API
+    async function getMoonPhase(){
+        const reponse =  await fetch("http://api.farmsense.net/v1/moonphases/?d="+toDayMoon);
+        const resultMoon = await reponse.json();
+        return resultMoon
+    }
+    
+    getMoonPhase().then(result => {
+        const moonPhase = result[0].Phase
+        balisePhase.innerHTML = moonPhase
 })
 
 
@@ -41,9 +55,8 @@ function gps(){
 }
 let coordArr = [];
 function stockPosition(position){
-     const lat = position.coords.latitude;
-     const long = position.coords.longitude;
-     coordArr.push(lat, long);
-     console.log(coordArr);
+    const lat = position.coords.latitude;
+    const long = position.coords.longitude;
+    coordArr.push(lat, long);
 };
 gps();
