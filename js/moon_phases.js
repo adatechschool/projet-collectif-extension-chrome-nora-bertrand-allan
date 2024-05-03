5921c50240ea5bed028d056d27a127328fa9aa36
Allan <allan.delatuillerie@gmail.com>
2024-05-03 13:57:24 +0200

    traduction des phases de la lune

---
+++ js/moon_phases.js
@@ -0,0 +1,10 @@
+const englishToFrench = {
+"New Moon" : "Nouvelle lune",
+"Waxing Crescent" : "Premier croissant",
+"First Quarter" : "Premier quartier",
+"Waxing Gibbous" : "Gibbeuse croissante",
+"Full Moon" : "Pleine lune",
+"Waning Gibbous" : "Gibbeuse décroissante",
+"Last Quarter" : "Dernier quartier",
+"Waning Crescent" : "Dernier croissant"
+}
