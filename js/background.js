3555899c8923c8270306d206b3470f01bff7c989
nora <noraidris621@gmail.com>
2024-05-07 11:56:36 +0200

    adding css animation ,imges file, manifest icon / description

--- js/background.js
+++ js/background.js
@@ -1,21 +1,22 @@
 async function getSun(coordArr) {
-    const reponse = await fetch("https://api.sunrise-sunset.org/json?lat=" + coordArr[0] + "&lng=" + coordArr[1] + "&tzid=Europe/Paris&formatted=0");
-    const resultSun = await reponse.json();
-    const sunset = resultSun.results.sunset.slice(11, 19);
-    const sunrise = resultSun.results.sunrise.slice(11, 19);
-    baliseSunrise.innerHTML = sunrise;
-    baliseSunset.innerHTML = sunset;
+    const reponse = await fetch("https://api.sunrise-sunset.org/json?lat=" + coordArr[0] + "&lng=" + coordArr[1] + "&tzid=Europe/Paris&formatted=0")
+    const resultSun = await reponse.json()
+    const sunset = resultSun.results.sunset.slice(11, 19)
+    const sunrise = resultSun.results.sunrise.slice(11, 19)
+    baliseSunrise.innerHTML = sunrise
+    baliseSunset.innerHTML = sunset
 }
 
 //MOON API
 async function getMoonPhase() {
-    const reponse = await fetch("http://api.farmsense.net/v1/moonphases/?d=" + toDayMoon);
+    const reponse = await fetch("http://api.farmsense.net/v1/moonphases/?d=" + toDayMoon)
     const result = await reponse.json();
-    return result;
+    return result
 }
 
 getMoonPhase().then(result => {
-    const moonPhase = result[0].Phase;
+    const moonPhase = result[0].Phase
     balisePhase.innerHTML = englishToFrench[moonPhase]
-});
+})
+
 
