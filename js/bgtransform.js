3c0b0efdae0d72fe6fd8968fa5722bbe283ebb67
Bertrand MARIE <photomb09@gmail.com>
2024-05-07 10:28:06 +0200

    ajout js background extension et add image phases moon

---
+++ js/bgtransform.js
@@ -0,0 +1,21 @@
+  function transformBg() {
+    const newDaySun = toDaySun.toString().slice(16,18);
+    console.log(Number(newDaySun));
+    const imgBack = document.querySelectorAll("div.main");
+    console.log(imgBack);
+    for (let element of imgBack) {
+        if ( 0 <= toDaySun < 6) {
+            element.classList.toggle("transformedDawn");
+        }
+        else if (6 <= toDaySun < 11) {
+            element.classList.toggle("transformedNoon");
+        }
+        else if (11 <= toDaySun < 18) {
+            element.classList.toggle("transformedDusk");
+        }
+        else if (18 <= toDaySun <= 23) {
+            element.classList.toggle("transformedDusk");
+        }
+    }
+}
+transformBg();
